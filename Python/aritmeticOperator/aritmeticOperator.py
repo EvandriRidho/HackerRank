d1ea3d0ef6b419554f3eee490a874148cf711f89
unknown <evandriridho555@gmail.com>
2025-02-04 17:09:04 +0700

    Add Py

---
+++ Python/aritmeticOperator/aritmeticOperator.py
@@ -0,0 +1,15 @@
+# Python - Aritmatika Operator
+
+def operator():
+    a = int(input("Masukkan nilai a : "))
+    b = int(input("Masukkan nilai b : "))
+
+    tambah = a + b
+    kurang = a - b
+    kali = a * b
+
+    print(tambah)
+    print(kurang)
+    print(kali)
+
+operator();
