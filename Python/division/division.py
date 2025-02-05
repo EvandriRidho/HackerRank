9248960a7793394d8117a3647a0d2d1957200fe4
unknown <evandriridho555@gmail.com>
2025-02-05 14:46:47 +0700

    Adding Python

---
+++ Python/division/division.py
@@ -0,0 +1,5 @@
+a = int(input("Masukkan nilai a : "))
+b = int(input("Masukkan nilai b : "))
+
+print(a // b)
+print(a / b)
---
+++ Python/loop/loop.py
@@ -0,0 +1,4 @@
+inputNum = int(input(("Masukkan Inpuut n : ")))
+
+for i in range(inputNum):
+    print(i * i)
